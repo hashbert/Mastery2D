2bb211788452452232bb81fe17180bdd3a2499dc
hashbert <obert.chen@gmail.com>
2022-12-20 22:55:10 -0500

    2-C
    jumping

--- Assets/Scripts/PlayerMovementController.cs
+++ Assets/Scripts/PlayerMovementController.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class PlayerMovementController : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 10f;
+    [SerializeField] private float _jumpForce = 400f;
+    private Rigidbody2D _rigidbody2D;
 
-    void Update()
+    private void Awake()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
     {
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(horizontal, vertical);
+        Vector3 movement = new Vector3(horizontal, 0);
 
         transform.position += movement * Time.deltaTime * _moveSpeed;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            _rigidbody2D.AddForce(Vector2.up * _jumpForce);
+        }
     }
 }
