663d4fcbc84edbbd83d944b5aaf0a6e5bd49dd2a
hashbert <obert.chen@gmail.com>
2022-12-26 01:40:49 -0500

    sprite flipping well done
    3F

--- Assets/Scripts/CharacterAnimation.cs
+++ Assets/Scripts/CharacterAnimation.cs
@@ -2,21 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(SpriteRenderer))]
+[RequireComponent(typeof(Animator))]
 public class CharacterAnimation : MonoBehaviour
 {
     private Animator _animator;
     private IMove _IMove;
+    private SpriteRenderer _spriteRenderer;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
         _IMove = GetComponent<PlayerMovementController>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float speed = _IMove.Speed;
-        _animator.SetFloat("Speed", speed);
+        _animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        if (speed != 0)
+        {
+            _spriteRenderer.flipX = speed > 0;
+        }
     }
 }
