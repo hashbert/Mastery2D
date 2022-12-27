e1a4c9a0d96c551885a4b1f672aab78d08fa5230
hashbert <obert.chen@gmail.com>
2022-12-27 15:40:49 -0500

    gamemanager singleton pattern
    4B

--- Assets/Scripts/KillOnTouch.cs
+++ Assets/Scripts/KillOnTouch.cs
@@ -10,7 +10,7 @@ public class KillOnTouch : MonoBehaviour
         var playerMovementController = collision.collider.GetComponent<PlayerMovementController>();
         if (playerMovementController != null)
         {
-            SceneManager.LoadScene(0);
+            GameManager.Instance.KillPlayer();
         }
     }
 }
