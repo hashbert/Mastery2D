77a067a14a2fbb0add13dc925b82b9b18e798424
hashbert <obert.chen@gmail.com>
2023-01-10 16:36:02 -0500

    sawblade lerping between two positions using update
    7E

---
+++ Assets/Scripts/SawBlade.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SawBlade : MonoBehaviour
+{
+    [SerializeField] private Transform _start;
+    [SerializeField] private Transform _end;
+    [SerializeField] private Transform _sawBladeSprite;
+    private float _positionPercent;
+    private int _direction = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _positionPercent += Time.deltaTime * _direction;
+        _sawBladeSprite.position = Vector3.Lerp(_start.position, _end.position, _positionPercent);
+
+        if (_positionPercent >=1 && _direction == 1)
+        {
+            _direction = -1;
+        }
+        else if (_positionPercent <=0 && _direction == -1)
+        {
+            _direction = 1;
+        }
+    }
+}
