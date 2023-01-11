56c5a4d736209b07ff0849e77619e38970ffe596
hashbert <obert.chen@gmail.com>
2023-01-10 22:39:35 -0500

    made MOVING PLATFORM by renaming old script more generically
    needs some work
    8A

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -2,20 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SawBlade : MonoBehaviour
+public class Mover : MonoBehaviour
 {
     [SerializeField] private Transform _start;
     [SerializeField] private Transform _end;
-    [SerializeField] private Transform _sawBladeSprite;
+    [SerializeField] private Transform _movingSprite;
     [SerializeField] private float _speed =1;
     private float _positionPercent;
     private int _direction = 1;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
@@ -24,7 +18,7 @@ public class SawBlade : MonoBehaviour
         float constantSpeed = _speed / distance;
 
         _positionPercent += Time.deltaTime * _direction * constantSpeed;
-        _sawBladeSprite.position = Vector3.Lerp(_start.position, _end.position, _positionPercent );
+        _movingSprite.position = Vector3.Lerp(_start.position, _end.position, _positionPercent );
 
         if (_positionPercent >=1 && _direction == 1)
         {
