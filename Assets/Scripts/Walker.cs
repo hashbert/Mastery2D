80c33af1001a468a2b7f34da08c740cfa82eb2ee
hashbert <obert.chen@gmail.com>
2023-01-11 01:41:07 -0500

    snail moves back and forth on platform fancy method for edge detection
    8E

--- Assets/Scripts/Walker.cs
+++ Assets/Scripts/Walker.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,6 +9,7 @@ public class Walker : MonoBehaviour
     private Collider2D _collider;
     private Rigidbody2D _rigidbody2D;
     private Vector2 _direction = Vector2.left;
+    private Transform _leftPosition;
 
     private void Awake()
     {
@@ -18,5 +20,35 @@ public class Walker : MonoBehaviour
     private void FixedUpdate()
     {
         _rigidbody2D.MovePosition(_rigidbody2D.position + _direction * _speed * Time.fixedDeltaTime);
+    }
+    private void LateUpdate()
+    {
+        if (ReachedEdge())
+        {
+            SwitchDirections();
+        }
+    }
+    private bool ReachedEdge()
+    {
+        float x = _direction.x == -1 ?
+            _collider.bounds.min.x - 0.1f :
+            _collider.bounds.max.x + 0.1f;
+
+        float y = _collider.bounds.min.y;
+
+        Vector2 origin = new Vector2(x, y);
+        Debug.DrawRay(origin, Vector2.down * 0.1f);
+
+        var hit = Physics2D.Raycast(origin, Vector2.down, 0.1f); 
+        if (hit.collider == null)
+        {
+            return true;
+        }
+
+        return false;
+    }
+    private void SwitchDirections()
+    {
+        _direction *= -1;
     }
 }
